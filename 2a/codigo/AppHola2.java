c0cbf8db1836e5eaf4b1944fd67dd46b1a7f155a
profaula <profaula@AA25A411P00.alumnos.upcont.es>
2020-01-20 09:52:29 +0100

    ok

---
+++ 2a/codigo/AppHola2.java
@@ -0,0 +1,46 @@
+public class AppHola2 {
+	
+	public static void main (String argv[])
+	{
+		Persona[] ps = new Persona[10];
+		ps[3] = new Persona("David","Perez","Gomez",17);
+
+		System.out.println(ps[3].getInfo("#"));
+
+		for (int i = 0; i< ps.length; i++)
+		{
+			if(ps[i] != null)
+				System.out.println("pos["+i+"]-"+ps[i].getInfo());
+		}
+
+		int i = 0;
+		while (i < ps.length){
+			if(ps[i] != null)
+				System.out.println("pos["+i+"]-"+ps[i].getInfo());
+			i++;
+		}
+
+		//BUCLE FOR EACH
+		for(Persona p : ps)
+			if(p != null)
+				System.out.println(p.getInfo());
+		
+
+		int [] ints = new int[20];
+		for (int j : ints)
+			System.out.print(j+",");
+
+		System.out.println();
+		double[][] ds = new double[3][5];
+		for (double[] f : ds)
+			for (double c : f)
+				System.out.print(c+",");
+		System.out.println();
+		for (int f = 0; f< ds.length; f ++)
+			for (int c = 0; c< ds[f].length; c ++)
+				System.out.print(ds[f][c]+",");
+
+	}
+
+
+}
